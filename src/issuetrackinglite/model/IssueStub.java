d231bba7c84b335e60aeb7b44874924605bb0f8d
alexnowak <alex.nowak@gmx.de>
2013-11-20 11:01:32 -0500

    Moved IssueStub sub-class into its own class

---
+++ src/issuetrackinglite/model/IssueStub.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package issuetrackinglite.model;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+
+/**
+ *
+ * @author anowak
+ */
+    // A Issue stub.
+    public class IssueStub implements ObservableIssue {
+        private final SimpleStringProperty id;
+        private final SimpleStringProperty projectName;
+        private final SimpleStringProperty title;
+        private final SimpleStringProperty description;
+        private final SimpleObjectProperty<Issue.IssueStatus> status =
+                new SimpleObjectProperty<Issue.IssueStatus>(Issue.IssueStatus.NEW);
+
+        IssueStub(String projectName, String id) {
+            this(projectName, id, null);
+        }
+        IssueStub(String projectName, String id, String title) {
+           // assert projectNames.contains(projectName);
+            //assert ! projectsMap.get(projectName).contains(id);
+            //assert ! issuesMap.containsKey(id);
+            this.projectName = new SimpleStringProperty(projectName);
+            this.id = new SimpleStringProperty(id);
+            this.title = new SimpleStringProperty(title);
+            this.description = new SimpleStringProperty("");
+        }
+
+        @Override
+        public Issue.IssueStatus getStatus() {
+            return status.get();
+        }
+
+        @Override
+        public String getId() {
+            return id.get();
+        }
+
+        @Override
+        public String getProjectName() {
+            return projectName.get();
+        }
+
+        @Override
+        public String getSynopsis() {
+            return title.get();
+        }
+
+        public void setSynopsis(String title) {
+            this.title.set(title);
+        }
+
+        @Override
+        public String getDescription() {
+            return description.get();
+        }
+
+        public void setDescription(String description) {
+            this.description.set(description);
+        }
+
+        public void setStatus(Issue.IssueStatus issueStatus) {
+            this.status.set(issueStatus);
+        }
+
+        @Override
+        public ObservableValue<String> idProperty() {
+            return id;
+        }
+
+        @Override
+        public ObservableValue<String> projectNameProperty() {
+            return projectName;
+        }
+
+        @Override
+        public ObservableValue<Issue.IssueStatus> statusProperty() {
+            return status;
+        }
+
+        @Override
+        public ObservableValue<String> synopsisProperty() {
+            return title;
+        }
+
+        @Override
+        public ObservableValue<String> descriptionProperty() {
+            return description;
+        }
+    }
+
--- src/issuetrackinglite/model/TrackingServiceStub.java
+++ src/issuetrackinglite/model/TrackingServiceStub.java
@@ -2,32 +2,6 @@
  * Copyright (c) 2012 Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
- * This file is available and licensed under the following license:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *  - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the distribution.
- *  - Neither the name of Oracle nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package issuetrackinglite.model;
 
@@ -37,9 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.MapChangeListener;
 import javafx.collections.MapChangeListener.Change;
@@ -75,90 +46,6 @@ public class TrackingServiceStub implements TrackingService {
         projectsMap.addListener(projectsMapChangeListener);
     }
 
-    // A Issue stub.
-    public final class IssueStub implements ObservableIssue {
-        private final SimpleStringProperty id;
-        private final SimpleStringProperty projectName;
-        private final SimpleStringProperty title;
-        private final SimpleStringProperty description;
-        private final SimpleObjectProperty<IssueStatus> status =
-                new SimpleObjectProperty<IssueStatus>(IssueStatus.NEW);
-
-        IssueStub(String projectName, String id) {
-            this(projectName, id, null);
-        }
-        IssueStub(String projectName, String id, String title) {
-            assert projectNames.contains(projectName);
-            assert ! projectsMap.get(projectName).contains(id);
-            assert ! issuesMap.containsKey(id);
-            this.projectName = new SimpleStringProperty(projectName);
-            this.id = new SimpleStringProperty(id);
-            this.title = new SimpleStringProperty(title);
-            this.description = new SimpleStringProperty("");
-        }
-
-        @Override
-        public IssueStatus getStatus() {
-            return status.get();
-        }
-
-        @Override
-        public String getId() {
-            return id.get();
-        }
-
-        @Override
-        public String getProjectName() {
-            return projectName.get();
-        }
-
-        @Override
-        public String getSynopsis() {
-            return title.get();
-        }
-
-        private void setSynopsis(String title) {
-            this.title.set(title);
-        }
-
-        @Override
-        public String getDescription() {
-            return description.get();
-        }
-
-        private void setDescription(String description) {
-            this.description.set(description);
-        }
-
-        private void setStatus(IssueStatus issueStatus) {
-            this.status.set(issueStatus);
-        }
-
-        @Override
-        public ObservableValue<String> idProperty() {
-            return id;
-        }
-
-        @Override
-        public ObservableValue<String> projectNameProperty() {
-            return projectName;
-        }
-
-        @Override
-        public ObservableValue<IssueStatus> statusProperty() {
-            return status;
-        }
-
-        @Override
-        public ObservableValue<String> synopsisProperty() {
-            return title;
-        }
-
-        @Override
-        public ObservableValue<String> descriptionProperty() {
-            return description;
-        }
-    }
 
     // You create new issue by adding a IssueStub instance to the issuesMap.
     // the new id will be automatically added to the corresponding list in
