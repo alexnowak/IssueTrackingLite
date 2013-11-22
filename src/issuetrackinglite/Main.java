7962c5ed5ce4bfeb0777543966077d051ff22f6f
Alex <alex.nowak@gmx.de>
2013-11-21 20:37:33 -0500

    Moved conf dir from src to ROOT

--- src/issuetrackinglite/Main.java
+++ src/issuetrackinglite/Main.java
@@ -5,8 +5,12 @@
  */
 package issuetrackinglite;
 
+import java.io.IOException;
+import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.Enumeration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,10 +23,40 @@ public class Main extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        logger.log(Level.INFO,"Launching JavaFX application.");
-        Application.launch(Main.class, (java.lang.String[])null);
+/**********    
+    public static void main(String[] args) throws IOException {
+        LogManager.getLogManager().readConfiguration();
+        
+        logger.severe("SEVERE");
+        logger.warning("WARNING");
+        logger.info("INFO");
+        logger.config("CONFIG");
+        logger.fine("FINE");
+        logger.finer("FINER");
+        logger.finest("FINEST");
+        
+        logger.info("Launching JavaFX application.");
+        logger.info("CWD: "+System.getProperty("user.dir"));
+
+        System.out.println("LogLevel: "+logger.getLevel());
+        for ( Enumeration p = System.getProperties().elements(); p.hasMoreElements();) 
+            System.out.println(p.nextElement());
+        System.out.println("Classpath: " + System.getProperty("java.class.path"));
+        System.out.println("java.util.logging.config.file: "+System.getProperty("java.util.logging.config.file"));
+        
+        Handler handlers[] = logger.getHandlers();
+        System.out.println("Found "+handlers.length+" logging handlers.");
+        for (int i=0; i<handlers.length; i++)
+            System.out.println("["+i+"] "+handlers[i]);
+
+        
+        System.out.println("LogManager props: "+LogManager.getLogManager().getProperty("handlers"));
+        
+
+        Application.launch(Main.class, (java.lang.String[])args);
     }
+   
+ ****/
     
     @Override
     public void stop() throws Exception{
@@ -31,7 +65,42 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        try {
+        LogManager.getLogManager().readConfiguration();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         logger.log(Level.INFO,"Starting JavaFX application.");
+        
+        logger.severe("SEVERE");
+        logger.warning("WARNING");
+        logger.info("INFO");
+        logger.config("CONFIG");
+        logger.fine("FINE");
+        logger.finer("FINER");
+        logger.finest("FINEST");
+        
+        logger.info("Launching JavaFX application.");
+        logger.info("CWD: "+System.getProperty("user.dir"));
+
+        System.out.println("LogLevel: "+logger.getLevel());
+        System.out.println("Classpath: " + System.getProperty("java.class.path"));
+        System.out.println("java.util.logging.config.file: "+System.getProperty("java.util.logging.config.file"));
+        System.getProperties().list(System.out);
+        //for ( Enumeration<String,String> p = (Enumeration<String,String>) System.getProperties().elements(); key.hasMoreElements();) 
+        //    System.out.println(key + ": "+System.getProperty(key.nextElement()));
+        
+        Handler handlers[] = logger.getHandlers();
+        System.out.println("Found "+handlers.length+" logging handlers.");
+        for (int i=0; i<handlers.length; i++)
+            System.out.println("["+i+"] "+handlers[i]);
+
+        
+        System.out.println("LogManager props: "+LogManager.getLogManager().getProperty("handlers"));
+
+        
+        
         try {
             AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("IssueTrackingLite.fxml"));
             Scene scene = new Scene(page);
