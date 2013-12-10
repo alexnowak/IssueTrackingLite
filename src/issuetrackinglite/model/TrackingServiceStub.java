4bee8dee97f66b0c023f3fe2944f9fa8e4e7fcf2
Alex <alex.nowak@gmx.de>
2013-12-10 18:22:26 -0500

    Added New/Rename/Delete buttons for projects

--- src/issuetrackinglite/model/TrackingServiceStub.java
+++ src/issuetrackinglite/model/TrackingServiceStub.java
@@ -35,7 +35,7 @@ public class TrackingServiceStub {
             while (rs.next()) {
                 Project proj = new Project(rs.getInt("Id"), rs.getString("Name"));
                 projs.add(proj);
-                logger.fine("\t["+ (nProj++) + "] Id" + proj.getId() + proj.getName());
+                logger.fine("\t["+ (nProj++) + "] Id" + proj.getId() + " - "+proj.getName());
             }
             logger.info("Number of projects in db: "+nProj);
         }
@@ -53,7 +53,6 @@ public class TrackingServiceStub {
                  Issue issue = new Issue(
                         rs.getInt("Id"),
                         proj.getId(),
-                        proj.getName(),
                         Issue.IssueStatus.values()[rs.getInt("Status")],
                         rs.getString("Synopsis"),
                         rs.getString("Description"));
@@ -64,7 +63,7 @@ public class TrackingServiceStub {
         return FXCollections.observableList(issues);
     }
 
-    private int getProjectId(String projName) throws SQLException {
+    public int getProjectId(String projName) throws SQLException {
         int projId;
         try (Statement s = Database.getInstance().getConnection().createStatement()) {
             ResultSet rs = s.executeQuery("select Id from Project where Name='" + projName + "'");
@@ -82,8 +81,64 @@ public class TrackingServiceStub {
         return projId;
     }
 
-    public void saveIssue(Issue issue) {
-        logger.severe("TODO: Implement saveIssue: Issue: "+issue);
+    public String getProjectName(int projId) throws SQLException {
+        String projName="";
+        try (Statement s = Database.getInstance().getConnection().createStatement()) {
+            ResultSet rs = s.executeQuery("select Name from Project where Id=" + projId);
+            if (!rs.next()) {
+                throw new SQLException("Project with ID" + projId + " not found in database.");
+            }
+
+            projName = rs.getString("Name");
+
+            if (rs.next()) {
+                throw new SQLException("Found more than one project with ID" + projId + " in database.");
+            }
+        }
+         // logger.finest("Project \"" + projName + " has ID " + projId);
+        return projName;
+    }
+
+    public int saveNewIssue(Issue issue) throws SQLException {
+        try (Statement s = Database.getInstance().getConnection().createStatement()) {
+            int result = s.executeUpdate(
+                    "insert into Issue values("
+                    + "DEFAULT, " + issue.getProjId() + ", "
+                    + Issue.IssueStatus.NEW.ordinal() + ", "
+                    + "'" + issue.getSynopsis() + "', "
+                    + "'" + issue.getDescription() + "'"
+                    + ")");
+            // get id of the saved issue
+            int newId = getLastIssueId();
+            issue.setId(newId);
+           logger.info("New issue saved successfully (SQLCode="+result+") Issue: "+issue);
+           return newId;
+        }
+    }
+
+    public int saveNewProject(Project proj) throws SQLException {
+        try (Statement s = Database.getInstance().getConnection().createStatement()) {
+            int result = s.executeUpdate(
+                    "insert into Project values("
+                    + "DEFAULT, " 
+                    + "'" + proj.getName() + "'"
+                    + ")" );
+            // get id of the saved issue
+            int newId = getLastProjectId();
+            proj.setId(newId);
+           logger.info("New projet saved successfully (SQLCode="+result+") Project: "+proj);
+           return newId;
+        }
+    }
+
+    public void updateIssue(Issue issue) throws SQLException {
+        try (Statement s = Database.getInstance().getConnection().createStatement()) {
+           int result = s.executeUpdate("update Issue set "
+                   +"Synopsis='"+issue.getSynopsis()+"',"
+                   +"Description='" + issue.getDescription() + "' "
+                   +"where Id="+issue.getId());
+           logger.info("Database update completed successfully (SQLCode="+result+") Issue: "+issue);
+        }
     }
 
 
@@ -92,7 +147,25 @@ public class TrackingServiceStub {
            int result = s.executeUpdate("delete from Issue where Id="+issue.getId());
            logger.info("Deleted from database: (SQLCode="+result+") Issue: "+issue);
         }
-        
+    }
+
+    public int getLastIssueId() throws SQLException {
+        return getLastId("Issue");
+    }
+    public int getLastProjectId() throws SQLException {
+        return getLastId("Project");
+    }
+
+    private int getLastId(String tableName) throws SQLException {
+        int lastId = -1;
+        try (Statement s = Database.getInstance().getConnection().createStatement()) {
+            ResultSet rs = s.executeQuery("select max(id) from "+tableName);
+            while (rs.next()) {
+                lastId = rs.getInt(1);
+            }
+        }
+        logger.fine("Table: " + tableName + " MaxId: "+lastId);
+        return lastId;
     }
 
     
