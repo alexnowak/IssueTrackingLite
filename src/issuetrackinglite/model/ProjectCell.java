c10e04e3f23705e98649b823335d6bfcba73cd38
Alex <alex.nowak@gmx.de>
2014-01-06 21:51:03 -0500

    minor changes

--- src/issuetrackinglite/model/ProjectCell.java
+++ src/issuetrackinglite/model/ProjectCell.java
@@ -3,87 +3,103 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package issuetrackinglite.model;
 
-import java.text.NumberFormat;
+import java.util.logging.Logger;
 import javafx.event.EventHandler;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
 
 /**
  *
  * @author Alex
  */
 public class ProjectCell extends ListCell<Project> {
-        private TextField textField;
+    static final Logger logger = Logger.getLogger(ProjectCell.class.getName());
 
-        @Override
-        public void startEdit() {
-            super.startEdit();
+    private TextField textField;
+    //private Project oldProject;
 
-            System.out.println("startEdit called");
+    public ProjectCell(Project p) {
+        setItem(p);
+        setText(p.getName());
+    }
 
-          if (textField == null) {
-              createTextField();
-          }
-         
-          setGraphic(textField);
-          setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-          textField.selectAll();
+    @Override
+    public void startEdit() {
+        super.startEdit();
+
+        logger.fine("startEdit called: Item="+getItem().getName()+" Id="+getItem().getProjId());
+
+        if (textField == null) {
+            createTextField();
         }
 
-        @Override
-        public void cancelEdit() {
-            super.cancelEdit();
-
-            System.out.println("cancelEdit called.");
-
-            setText(getItem().toString());
-            setContentDisplay(ContentDisplay.TEXT_ONLY);
-        }
-
-        
-
-        @Override
-        public void updateItem(Project item, boolean empty) {
-            super.updateItem(item, empty);
-            System.out.println("updateItem: project=" + item + " empty=" + empty);
-
-            setText(item == null ? "" : item.getName() );
-
-//            if (item != null) {
-//                double value = item.doubleValue();
-//
-//                setTextFill(value == 0 ? Color.BLACK
-//                        : value < 0 ? Color.RED : Color.GREEN);
-//            }
-        }
-
-        private void createTextField() {
-            textField = new TextField(getString());
-            textField.setMinWidth(this.getWidth() - this.getGraphicTextGap() * 2);
-            textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
-                @Override
-                public void handle(KeyEvent t) {
-                    System.out.println("handle KeyEvent called: " + t);
-
-                    if (t.getCode() == KeyCode.ENTER) {
-                        commitEdit( new Project(textField.getText()));
-                    } else if (t.getCode() == KeyCode.ESCAPE) {
-                        cancelEdit();
-                    }
-                }
-            });
-        }
-
-        private String getString() {
-            return getItem() == null ? "" : getItem().toString();
-        }
+        setGraphic(textField);
+        setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        textField.selectAll();
+    }
     
+    @Override
+    public void commitEdit(Project newValue) {
+        super.commitEdit(newValue);
+        logger.fine("commitEdit: newValue="+newValue + " ID="+newValue.getProjId());
+        
+        if (newValue.getName().equals(getItem().getName())) {
+            logger.fine("Project name has not changed.");
+            return;
+        }
+       
+        logger.fine("Renaming Project FROM \""+ getItem().getName() + "\" TO \"" + newValue.getName() +"\"...");
+                
+    }
+    
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+
+        logger.fine("cancelEdit called: Item="+getItem());
+
+        setText(getItem().toString());
+        setContentDisplay(ContentDisplay.TEXT_ONLY);
+    }
+
+    
+    @Override
+    public void updateItem(Project item, boolean empty) {
+        super.updateItem(item, empty);
+        
+        if (item==null || empty)
+            return;
+        logger.fine("updateItem: project=" + item + " empty=" + empty);
+        setText(item.getName());
+
+    }
+    
+    
+    private void createTextField() {
+        textField = new TextField(getString());
+        textField.setMinWidth(this.getWidth() - this.getGraphicTextGap() * 2);
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent t) {
+                logger.finer("handle KeyEvent called: " + t);
+
+                if (t.getCode() == KeyCode.ENTER) {
+                    commitEdit(new Project(textField.getText()));
+                } else if (t.getCode() == KeyCode.ESCAPE) {
+                    cancelEdit();
+                }
+            }
+        });
+    }
+
+    private String getString() {
+        return getItem() == null ? "" : getItem().toString();
+    }
+
 }
